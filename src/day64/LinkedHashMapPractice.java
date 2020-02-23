500d5028c85fdc7b1f6f6580a1f2744ecd6ead6a
Mariia Lukianenko <lukianenko.mariia@gmail.com>
2020-02-23 15:57:48 -0600

    1

---
+++ src/day64/LinkedHashMapPractice.java
@@ -0,0 +1,24 @@
+package day64;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapPractice {
+
+    public static void main(String[] args) {
+
+        // use LinkedHashMap if you want the insertion (porydok) order
+
+        Map<String, Double> groceryPriceMap = new LinkedHashMap<>();
+        groceryPriceMap.put("Tomato", 1.99);
+        groceryPriceMap.put("Patato", 2.99);
+        groceryPriceMap.put("Grape", 3.99);
+        groceryPriceMap.put("Banana", 1.99);
+        groceryPriceMap.put("Apple", 1.89);
+
+        System.out.println("groceryPriceMap = " + groceryPriceMap);
+
+
+    }
+
+}
